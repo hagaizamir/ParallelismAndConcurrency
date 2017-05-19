928d04242997a535c4fc2f97201e46128d5ae27e
Hagai Zamir <hagai34@gmail.com>
2017-05-19 12:29:04 +0300

    single tread executor

--- src/hagai/edu/Main.java
+++ src/hagai/edu/Main.java
@@ -2,34 +2,50 @@ package hagai.edu;
 
 import hagai.edu.SheepHerd;
 
+import java.time.LocalTime;
 import java.util.ArrayList;
-import java.util.concurrent.Callable;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 import java.util.function.Consumer;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        
+        SheepHerd herd = new SheepHerd();
+        ExecutorService pool = Executors.newFixedThreadPool(4)  ;
+        pool.execute(herd::addSheepAndCount);
 
-//        for (int i = 0; i < 10; i++) {
-//            new Thread(() -> {
-//            }).start();
-//        }
-        ExecutorService service = Executors.newSingleThreadExecutor();
-        service.execute(()->  System.out.println("Running the task...."));
-        System.out.println("Main");
-        service.execute(()->  System.out.println("Running the task...."));
-         System.out.println("Main");
-        service.execute(()->  System.out.println("Running the task...."));
-         System.out.println("Main");    
-        service.execute(()->  System.out.println("Running the task...."));
-         System.out.println("Main");    
+       ScheduledExecutorService sched =  Executors.newScheduledThreadPool(1)    ;
+       sched.scheduleAtFixedRate(()->{
+           System.out.println(LocalTime.now());
+
+           },1,3,TimeUnit.SECONDS);
+       pool.execute(herd::addSheepAndCount);
+      
+
+
+        ArrayList<String> data = new ArrayList<>();
+        data.add("A");
+        data.add("B");
+        data.add("C");
+        data.add("D");
+
+        data.forEach(Main::readData)  ;
+
+
+        ExecutorService service = Executors.newFixedThreadPool(4);
+        service.execute(Main::doStuff);
 
         service.shutdown();
 
+
+    }
+    public static void  doStuff (){
+        System.out.println("Doing Stuff");
+    }
+    public static void  readData(String s){
+        System.out.println("Read" + s);
 
     }
    }
