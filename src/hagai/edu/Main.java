9655c21790ce64fa708447417f02796bebf511d6
Hagai Zamir <hagai34@gmail.com>
2017-05-19 11:28:23 +0300

    Synchronize

--- src/hagai/edu/Main.java
+++ src/hagai/edu/Main.java
@@ -1,4 +1,5 @@
-package hagai.edu;
+ package hagai.edu;
+import hagai.edu.SheepHerd;
 
 import java.util.ArrayList;
 import java.util.concurrent.Callable;
@@ -7,40 +8,29 @@ import java.util.function.Consumer;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         SheepHerd herd = new SheepHerd();
-        for (int i = 0; i <10 ; i++) {
-            new Thread(()->herd.addSheepAndCount(),String.valueOf(i)).start();
 
+        for (int i = 0; i < 10; i++) {
+            new Thread(()->herd.addSheepAndCount(), String.valueOf(i)).start();
         }
 
-//        //will make the main tread sleep
-//        //Thread.sleep(1000);
-//
-//        //favour encapsulation over inheritance
-//
-//        Thread t = new Thread(() -> {
-//
-//            while (true){
-//                System.out.println("Hello");
-//                //will make the 2nd thread sleep
-//                try {
-//                    Thread.sleep(5000);//don't use the cpu
-//                } catch (InterruptedException e) {
-//                    //get rid of any resources and make the tread stop
-//                    //clean up
-//                    e.printStackTrace();
-//                }
-//            }
-//
-//        });
-//        t.start();
-//
-//      //  t.interrupt();
-//
-//
-//
-//
+    }
 
+    final static Object LOCK = new Object();
+
+
+    public static void doSomeWork() throws InterruptedException {
+        System.out.println(Thread.currentThread().getName());
+        Thread.sleep(1000);
+
+        //Thread safe.
+        synchronized (LOCK){
+            //atomic unit of execution.
+            //increment your counter safely
+            System.out.println(Thread.currentThread().getName() + " IS Alone here");
+        }
+
+        Thread.sleep(1000);
+        System.out.println(Thread.currentThread().getName());
     }
 }
--- src/hagai/edu/SheepHerd.java
+++ src/hagai/edu/SheepHerd.java
@@ -6,8 +6,15 @@ package hagai.edu;
 public class SheepHerd {
     private int sheepCounter = 0;
 
+
     public synchronized void  addSheepAndCount (){
         System.out.print(++sheepCounter+"Thread ");
+        System.out.println(Thread.currentThread().getName());
+    }
+    public void  addSheepAndCountExplicit () {
+        synchronized (this) {
+        }
+        System.out.print(++sheepCounter + "Thread ");
         System.out.println(Thread.currentThread().getName());
     }
 }
