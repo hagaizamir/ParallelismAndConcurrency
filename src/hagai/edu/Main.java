0dc6aaecfd67c3cf31ac7928656c30c6a33823a4
Hagai Zamir <hagai34@gmail.com>
2017-05-19 11:40:21 +0300

    single tread executor

--- src/hagai/edu/Main.java
+++ src/hagai/edu/Main.java
@@ -1,36 +1,35 @@
- package hagai.edu;
+package hagai.edu;
+
 import hagai.edu.SheepHerd;
 
 import java.util.ArrayList;
 import java.util.concurrent.Callable;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.function.Consumer;
 
 public class Main {
 
     public static void main(String[] args) {
-        SheepHerd herd = new SheepHerd();
 
-        for (int i = 0; i < 10; i++) {
-            new Thread(()->herd.addSheepAndCount(), String.valueOf(i)).start();
-        }
+
+//        for (int i = 0; i < 10; i++) {
+//            new Thread(() -> {
+//            }).start();
+//        }
+        ExecutorService service = Executors.newSingleThreadExecutor();
+        service.execute(()->  System.out.println("Running the task...."));
+        System.out.println("Main");
+        service.execute(()->  System.out.println("Running the task...."));
+         System.out.println("Main");
+        service.execute(()->  System.out.println("Running the task...."));
+         System.out.println("Main");    
+        service.execute(()->  System.out.println("Running the task...."));
+         System.out.println("Main");    
+
+        service.shutdown();
+
 
     }
-
-    final static Object LOCK = new Object();
-
-
-    public static void doSomeWork() throws InterruptedException {
-        System.out.println(Thread.currentThread().getName());
-        Thread.sleep(1000);
-
-        //Thread safe.
-        synchronized (LOCK){
-            //atomic unit of execution.
-            //increment your counter safely
-            System.out.println(Thread.currentThread().getName() + " IS Alone here");
-        }
-
-        Thread.sleep(1000);
-        System.out.println(Thread.currentThread().getName());
-    }
-}
+   }
